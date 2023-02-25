dc6e16fa87795c27f18c310a300c5d0e41ec8df8
ChrisZaharenkova <kristina.zaharenkova00@mail.ru>
2023-02-25 15:55:38 +0300

    .

--- README.md
+++
@@ -1,1 +0,0 @@
-# site for cosmetic brand
--- js/script.js
+++ js/script.js
@@ -5,3 +5,10 @@ $(document).ready(function(){
 	});
 
 });
+var form = document.querySelector('form');
+form.onsubmit = function ()
+  {
+  var text = form.text;
+  text.value = '<p>' + text.value + '</p>';
+  text.value = text.value.replace(/\n/g, '</p><p>');
+  };
